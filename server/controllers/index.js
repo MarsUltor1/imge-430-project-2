75908daeeedf9f3ca65e8029f8a422dfbe9d1fd1
Mark Grimke <MarsAltar01@gmail.com>
2023-12-11 18:09:18 -0500

    finished adding 404 page

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,3 @@
 module.exports.Account = require('./Account.js');
 module.exports.Tweet = require('./Tweet.js');
-
-module.exports.get404 = () => {
-    
-}
+module.exports.get404 = (req, res) => res.render('404');
